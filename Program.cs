6ec904f43b4cae606fa82e763f77b52f9bd4c4b3
thur04kalinauskas@gmail.com <e107ti@etec.sp;gov;br>
2023-03-03 21:45:04 -0300

    Queria ouvir a Musica :(

--- Program.cs
+++ Program.cs
@@ -3,12 +3,12 @@ Console.WriteLine("Ré"); Console.Beep(1485, 200 );
 Console.WriteLine("Mi"); Console.Beep(1650, 200);
 Console.WriteLine("Fá"); Console.Beep(1759, 200);
 
-    Thread.Sleep(600);
+    Thread.Sleep(700);
 
 Console.WriteLine("Fá"); Console.Beep(1759, 200);
 Console.WriteLine("Fá");Console.Beep(1759, 200);
 
-    Thread.Sleep(1200);
+    Thread.Sleep(1400);
 
 
 Console.WriteLine("Dó"); Console.Beep(1320, 200);
@@ -16,13 +16,13 @@ Console.WriteLine("Ré"); Console.Beep(1485, 200 );
 Console.WriteLine("Dó"); Console.Beep(1320, 200);
 Console.WriteLine("Ré"); Console.Beep(1485, 200);
 
-    Thread.Sleep(600);
+    Thread.Sleep(700);
 
 Console.WriteLine("Ré"); Console.Beep(1485, 200);
 Console.WriteLine("Ré"); Console.Beep(1485, 200);
 
 
-    Thread.Sleep(1200);
+    Thread.Sleep(1400);
 
 
 Console.WriteLine("Dó"); Console.Beep(1320, 200);
@@ -30,19 +30,19 @@ Console.WriteLine("Sol"); Console.Beep(1980, 200);
 Console.WriteLine("Fá"); Console.Beep(1759,200);
 Console.WriteLine("Mi"); Console.Beep(1650,200);
 
-    Thread.Sleep(600);
+    Thread.Sleep(700);
 
 Console.WriteLine("Mi"); Console.Beep(1650,200);
 Console.WriteLine("Mi"); Console.Beep(1650,200);
 
-    Thread.Sleep(1200);
+    Thread.Sleep(1400);
 
 Console.WriteLine("Dó"); Console.Beep(1320, 200);
 Console.WriteLine("Ré"); Console.Beep(1485, 200);
 Console.WriteLine("Mi"); Console.Beep(1650,200);
 Console.WriteLine("Fá"); Console.Beep(1759,200);
 
-    Thread.Sleep(600);
+    Thread.Sleep(700);
 
 Console.WriteLine("Fá"); Console.Beep(1759,200);
 Console.WriteLine ("Fá"); Console.Beep(1759,200);
